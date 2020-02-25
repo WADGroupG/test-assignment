dee3afe40e3cf5268ed8234dd41d1159cca1e6be
brianchan98 <60928149+brianchan98@users.noreply.github.com>
2020-02-25 13:45:00 +0000

    Add files via upload

---
+++ wad_dond_gen_01.rb
@@ -0,0 +1,56 @@
+# Ruby code file - All your code should be located between the comments provided.
+
+# Main class module
+module DOND_Game
+	# Input and output constants processed by subprocesses. MUST NOT change.
+	GOES = 5
+
+	class Game
+		attr_reader :sequence, :selectedboxes, :openedboxes, :chosenbox, :selectedbox, :turn, :input, :output, :winner, :played, :wins, :losses, :guess, :values, :amounts
+		attr_writer :sequence, :selectedboxes, :openedboxes, :chosenbox, :selectedbox, :turn, :input, :output, :winner, :played, :wins, :losses, :guess, :values, :amounts
+
+		def initialize(input, output)
+			@input = input
+			@output = output
+		end
+		
+		def getinput
+			@input.gets.chomp.upcase
+		end
+		
+		def storeguess(guess)
+			if guess != ""
+				@selectedboxes = @selectedboxes.to_a.push "#{guess}"
+			end
+		end
+		
+		# Any code/methods aimed at passing the RSpect tests should be added below.
+		
+		def start
+			@output.puts "Welcome to Deal or No Deal!"
+			@output.puts "Designed by: #{self.created_by}"
+			@output.puts "StudentID: #{self.student_id}"
+			@output.puts "Starting game..."
+		end
+		
+		def created_by
+			return "Brian Chan"
+		end
+		
+		def student_id
+			return "51989517"
+		end
+
+		def displaymenu
+			@output.puts "Menu: (1) Play | (2) New | (3) Analysis | (9) Exit"
+		end 
+		
+		def resetgame
+		end
+
+		# Any code/methods aimed at passing the RSpect tests should be added above.
+
+	end
+end
+
+
